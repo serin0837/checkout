7767c89a93637b5a2ef5307e832830ab7b2d9254
Serion Jeon <serin@totalprocessing.com>
2024-03-19 18:16:20 +0000

    refactor test to table test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -93,143 +93,141 @@ func TestScan(t *testing.T) {
 }
 
 func TestTotalPrice(t *testing.T) {
-	testSKU := []models.Item{
+
+	// Table tests
+	testCases := []struct {
+		items       []models.Item
+		expectedNum int
+	}{
+		// Test1
 		{
-			Name:         "A",
-			UnitPrice:    50,
-			SpecialPrice: 130,
-			Number:       1,
+			[]models.Item{
+				{
+					Name:         "A",
+					UnitPrice:    50,
+					SpecialPrice: 130,
+					Number:       1,
+				},
+				{
+					Name:         "B",
+					UnitPrice:    30,
+					SpecialPrice: 45,
+					Number:       1,
+				},
+				{
+					Name:         "C",
+					UnitPrice:    20,
+					SpecialPrice: 0,
+					Number:       1,
+				},
+				{
+					Name:         "D",
+					UnitPrice:    15,
+					SpecialPrice: 0,
+					Number:       1,
+				},
+			},
+			115,
 		},
+		// Test2
 		{
-			Name:         "B",
-			UnitPrice:    30,
-			SpecialPrice: 45,
-			Number:       1,
+			[]models.Item{
+				{
+					Name:         "A",
+					UnitPrice:    50,
+					SpecialPrice: 130,
+					Number:       15,
+				},
+				{
+					Name:         "B",
+					UnitPrice:    30,
+					SpecialPrice: 45,
+					Number:       20,
+				},
+				{
+					Name:         "C",
+					UnitPrice:    20,
+					SpecialPrice: 0,
+					Number:       30,
+				},
+				{
+					Name:         "D",
+					UnitPrice:    15,
+					SpecialPrice: 0,
+					Number:       60,
+				},
+			}, 2850,
 		},
+		// Test3
 		{
-			Name:         "C",
-			UnitPrice:    20,
-			SpecialPrice: 0,
-			Number:       1,
+			[]models.Item{
+				{
+					Name:          "A",
+					UnitPrice:     50,
+					SpecialPrice:  130,
+					SpecialNumber: 3,
+					Number:        3,
+				},
+				{
+					Name:          "B",
+					UnitPrice:     30,
+					SpecialPrice:  45,
+					SpecialNumber: 2,
+					Number:        2,
+				},
+				{
+					Name:         "C",
+					UnitPrice:    20,
+					SpecialPrice: 0,
+					Number:       1,
+				},
+				{
+					Name:         "D",
+					UnitPrice:    15,
+					SpecialPrice: 0,
+					Number:       1,
+				},
+			}, 210,
 		},
+		// Test4
 		{
-			Name:         "D",
-			UnitPrice:    15,
-			SpecialPrice: 0,
-			Number:       1,
+			[]models.Item{
+				{
+					Name:          "A",
+					UnitPrice:     50,
+					SpecialPrice:  130,
+					SpecialNumber: 3,
+					Number:        15,
+				},
+				{
+					Name:          "B",
+					UnitPrice:     30,
+					SpecialPrice:  45,
+					SpecialNumber: 2,
+					Number:        20,
+				},
+				{
+					Name:          "C",
+					UnitPrice:     20,
+					SpecialPrice:  0,
+					SpecialNumber: 0,
+					Number:        1,
+				},
+				{
+					Name:          "D",
+					UnitPrice:     15,
+					SpecialPrice:  0,
+					SpecialNumber: 0,
+					Number:        1,
+				},
+			}, 1135,
 		},
 	}
 
-	got := TotalPrice(testSKU)
-	expected := 115
-	if got != expected {
-		t.Errorf("total price should calculate as expected. got: %v expected: %v", got, expected)
+	for _, tt := range testCases {
+		got := TotalPrice(tt.items)
+		if got != tt.expectedNum {
+			t.Errorf("total price should calculate as expected. got: %v expected: %v", got, tt.expectedNum)
+		}
 	}
-
-	testSKU2 := []models.Item{
-		{
-			Name:         "A",
-			UnitPrice:    50,
-			SpecialPrice: 130,
-			Number:       15,
-		},
-		{
-			Name:         "B",
-			UnitPrice:    30,
-			SpecialPrice: 45,
-			Number:       20,
-		},
-		{
-			Name:         "C",
-			UnitPrice:    20,
-			SpecialPrice: 0,
-			Number:       30,
-		},
-		{
-			Name:         "D",
-			UnitPrice:    15,
-			SpecialPrice: 0,
-			Number:       60,
-		},
-	}
-
-	got2 := TotalPrice(testSKU2)
-	expected2 := 2850
-	if got2 != expected2 {
-		t.Errorf("total price should calculate as expected here. got: %v expected: %v", got2, expected2)
-	}
-
-	// Test case for discount price
-	testSKU3 := []models.Item{
-		{
-			Name:          "A",
-			UnitPrice:     50,
-			SpecialPrice:  130,
-			SpecialNumber: 3,
-			Number:        3,
-		},
-		{
-			Name:          "B",
-			UnitPrice:     30,
-			SpecialPrice:  45,
-			SpecialNumber: 2,
-			Number:        2,
-		},
-		{
-			Name:         "C",
-			UnitPrice:    20,
-			SpecialPrice: 0,
-			Number:       1,
-		},
-		{
-			Name:         "D",
-			UnitPrice:    15,
-			SpecialPrice: 0,
-			Number:       1,
-		},
-	}
-
-	got3 := TotalPrice(testSKU3)
-	expected3 := 210
-	if got3 != expected3 {
-		t.Errorf("total price should calculate as expected. got: %v expected: %v", got3, expected3)
-	}
-
-	testSKU4 := []models.Item{
-		{
-			Name:          "A",
-			UnitPrice:     50,
-			SpecialPrice:  130,
-			SpecialNumber: 3,
-			Number:        15,
-		},
-		{
-			Name:          "B",
-			UnitPrice:     30,
-			SpecialPrice:  45,
-			SpecialNumber: 2,
-			Number:        20,
-		},
-		{
-			Name:          "C",
-			UnitPrice:     20,
-			SpecialPrice:  0,
-			SpecialNumber: 0,
-			Number:        1,
-		},
-		{
-			Name:          "D",
-			UnitPrice:     15,
-			SpecialPrice:  0,
-			SpecialNumber: 0,
-			Number:        1,
-		},
-	}
-
-	got4 := TotalPrice(testSKU4)
-	expected4 := 1135
-	if got4 != expected4 {
-		t.Errorf("total price should calculate as expected. got: %v expected: %v", got4, expected4)
-	}
-
 }
